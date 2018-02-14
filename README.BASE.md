f0da89a1853dd40699fb23b595b9081cdb1675ea
Daniel Gong <coolgk@gmail.com>
2018-02-14 13:52:16 +0000

    init update

---
+++ README.BASE.md
@@ -0,0 +1,207 @@
+# @coolgk/mongo
+
+`npm install @coolgk/mongo`
+
+A simple, lightweight javascript / typescript nodejs mvc framework that helps you to create object oriented, modular and testable code.
+
+[![Build Status](https://travis-ci.org/coolgk/node-mvc.svg?branch=master)](https://travis-ci.org/coolgk/node-mvc) [![Coverage Status](https://coveralls.io/repos/github/coolgk/node-mvc/badge.svg?branch=develop)](https://coveralls.io/github/coolgk/node-mvc?branch=develop) [![dependencies Status](https://david-dm.org/coolgk/node-mvc/status.svg)](https://david-dm.org/coolgk/node-mvc) [![Known Vulnerabilities](https://snyk.io/test/github/coolgk/node-mvc/badge.svg)](https://snyk.io/test/github/coolgk/node-mvc)
+
+## Documentation
+
+This framework routes HTTP requests to class methods.
+
+e.g.
+**"GET /shop/product/description/1"** calls the **"description"** method in "/modules/**shop**/controllers/**product**.js" (see example code below)
+
+In this example request, **"shop"** is a module (folder), **"product"** is a controller (file), **"description"** is an action (method) and **"1"** is a parameter. The format of the request is **/module/[controller]/[action]/[param]**
+
+The framework looks for files from the folder structure below.
+
+    ./index.js
+    ./modules
+        /shop
+            /controllers
+                product.js
+                anothercontroller.js
+            /models
+                model.js
+        /anothermodule
+            /controllers
+            ...
+
+### Controller
+
+The controller module must export a **"default"** property which is a class that extends the base **"Controller"** class from @coolgk/mvc/controller. Folder (module), file (controller) and method (action) names must be in lowercase without special characters except for hyphens and numbers `/[a-z0-9\-]/` or in camelCase if a request contains hyphens e.g. **action-one** is converted to **actionOne**
+
+**product.js** controller example
+
+```javascript
+const { Controller } = require('@coolgk/mvc/controller');
+
+class Product extends Controller {
+    /**
+     * @param {object} dependencies - this param is destructured in this example
+     * @param {object} dependencies.params - url param values based on the patterns configured in getRoutes()
+     * @param {object} dependencies.globals - the object passed into the router's constructor
+     * @param {*} dependencies.services - services returned by getServices()
+     */
+    description ({ params, services, globals }) {
+        // globals contains global dependencies passed into the router class (see example below)
+        globals.response.json(
+            services.model.find(params.id)
+        );
+    }
+
+    /**
+     * setup valid routes to methods
+     */
+    getRoutes () {
+        return {
+            GET: {
+                description: ':id' // allow GET request to access the description() method
+            }
+        }
+    }
+
+    /**
+     * setup local dependencies
+     */
+    getServices () {
+        return {
+            model: new (require('../models/model.js'))()
+        };
+    }
+
+    /**
+     * setup permission callbacks for accessing methods
+     */
+    getPermissions () {
+        return {
+            // * the is default permission for all methods in this class
+            // can be used for checking app level permissions e.g. login sessions etc.
+            '*': () => false, // false = deny all by default
+            // true or Promise<true>: skip permission check for the description() method
+            'description': () => true
+        };
+    }
+}
+
+exports.default = Product;
+```
+
+### Entry Point (Router)
+
+index.js / server.js however you name it...
+
+An example of using express with this framework
+
+```javascript
+const express = require('express');
+const { Router } = require('@coolgk/mvc/router');
+
+const app = express();
+
+app.use(async (request, response, next) => {
+
+    // initialise router
+    const router = new Router({
+        rootDir: __dirname, // required
+        url: request.originalUrl, // required
+        method: request.method, // required
+        response // you can pass anything into router, these variables are injected into controllers methods in globals
+    });
+
+    // router.route() returns the return value of the controller method if the return value is not falsy
+    // otherwise it returns an object formatted by the "response" object (see the documention for @coolgk/mvc/response at the bottom)
+    // e.g. { code: 200, text: 'SUCCESS' }, { code: 200, json: {...} }, { code: 200, file: { name: ..., path: ... } } etc.
+    const result = (await router.route());
+
+    // for handling 404 / 403 returned from the router
+    result && result.code && response.status(result.code).send(result.text);
+
+});
+
+app.listen(3000);
+```
+
+### Unit Test
+
+Dependencies are injected into methods, you can easily mock them in your tests.
+
+```javascript
+'use strict';
+
+const sinon = require('sinon');
+const expect = require('chai').expect;
+
+describe('Test Example', function () {
+    // this test is for the example code in https://github.com/coolgk/node-mvc/tree/master/src/examples
+    // i.e. not the product.js controller above
+    const ControllerClass = require(`../javascript/modules/example/controllers/extended`).default;
+
+    let controller;
+    let params;
+    let response;
+    let services;
+    let globals;
+
+    beforeEach(() => {
+        // initialise controller for each test case
+        controller = new ControllerClass();
+        // setup dependencies
+        params = { id: 123 };
+        // create test spy on global dependency: response
+        response = {
+            json: sinon.spy()
+        };
+        // create test stub on local dependency: services
+        services = {
+            model: {
+                getUser: sinon.stub().returns({ name: 'abc' })
+            }
+        };
+        // create test stub on global dependency: globals
+        globals = {
+            session: {
+                getAll: sinon.stub().returns({ session: 'data' })
+            }
+        };
+    });
+
+    it('should show user name and session', async () => {
+        await controller.user({ params, response, services, globals });
+        expect(services.model.getUser.calledWithExactly(params.id)).to.be.true;
+        expect(globals.session.getAll.calledOnce).to.be.true;
+        expect(response.json.calledWithExactly({
+            user: { name: 'abc' },
+            session: { session: 'data' }
+        })).to.be.true;
+    });
+
+});
+```
+
+### More Examples
+
+[JavaScript Examples](https://github.com/coolgk/node-mvc/tree/master/src/examples/javascript)
+
+- A simple app
+  - [Entry Point](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.express.simple.js)
+  - [Controller File](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/modules/example/controllers/simple.js)
+- An example that decouples express from controllers
+  - [Entry Point](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.express.decoupled.js)
+  - [Controller File](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/modules/example/controllers/decoupled.js)
+- An example with session and form data handlers
+  - [Entry Point](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.express.extended.js)
+  - [Controller File](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/modules/example/controllers/extended.js)
+- A native node app without express
+  - [Entry Point](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.native.js)
+  - [Controller File](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/modules/example/controllers/extended.js)
+- An example of using RabbitMQ for managing requests
+  - [message pusher](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.express.rabbitmq.publisher.js)
+  - [single task consumer](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.rabbitmq.consumer.single.task.js)
+  - [catch all consumer](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/index.rabbitmq.consumer.default.js)
+  - [controller](https://github.com/coolgk/node-mvc/blob/master/src/examples/javascript/modules/example/controllers/decoupled.js)
+
+[TypeScript Examples](https://github.com/coolgk/node-mvc/tree/master/src/examples/typescript)
+TypeScript version of the examples above
---
+++ src/mongo.ts
@@ -0,0 +1,469 @@
+import { Db, ObjectID, Cursor, Collection, FindOneOptions } from 'mongodb';
+import { toArray } from '@coolgk/array';
+
+// model field data types
+export enum DataType {
+    STRING = 'string',
+    NUMBER = 'number',
+    OBJECT = 'object',
+    ENUM = 'enum',
+    BOOLEAN = 'boolean',
+    DATE = 'date',
+    OBJECTID = 'objectid'
+}
+
+// model field definition
+export interface IFieldSchema {
+    type: DataType;
+    setter?: (value: any) => any;
+    enum?: (string | number | boolean)[];
+    default?: any;
+    model?: typeof Mongo;
+    object?: ISchema;
+    array?: boolean;
+}
+
+// model fields
+export interface ISchema {
+    [field: string]: IFieldSchema;
+}
+
+// query result
+export interface IResult {
+    [field: string]: any;
+}
+
+// query join definition
+export interface IJoin {
+    // [index: string]: {
+    //     fields?: {
+    //         [index: string]: 1 | 0
+    //     },
+    //     filters?: {
+    //         [index: string]: any
+    //     },
+    //     join?: IJoin,
+    //     data?: IResult[]
+    // };
+    on: string | string[];
+    projection?: {
+        [field: string]: 1 | 0
+    };
+    filters?: IQuery;
+    join?: IJoin[];
+    data?: Cursor;
+    model?: typeof Mongo;
+}
+
+// reference pointer type for find ref in data
+export interface IReferencePointer {
+    parent: IResult | IResult[];
+    field: string | number;
+    path: string[];
+    // id?: ObjectID;
+}
+
+// object id fields in search result
+export interface IObjectIdInData {
+    // [collection: string]: {
+    //     model: typeof Mongo;
+    //     dbRefsById: {
+    //         [id: string]: IReferencePointer[]
+    //     }
+    // };
+    [field: string]: IReferencePointer[];
+    // {
+    //     [id: string]: IReferencePointer[]
+    // };
+}
+
+// join.data indexed by collection and field
+// export interface IJoinCollectionData {
+//     [collection: string]: {
+//         [field: string]: {
+//             [id: string]: IResult
+//         }
+//     };
+// }
+
+// all object id fields found in field config
+// export interface IObjectIdFields {
+//     [index: string]: any;
+// }
+
+// mongo query object
+export interface IQuery {
+    [index: string]: any;
+}
+
+// data found in referenced collections
+// export interface IFilteredObjectIdData {
+//     [index: string]: any[];
+// }
+
+// model mapping for collections in join
+// export interface ICollectionModel {
+//     [index: string]: typeof Mongo;
+// }
+
+export interface IFindOptions extends FindOneOptions {
+    join?: IJoin[];
+    cursor?: boolean;
+}
+
+export interface IConfig {
+    db: Db;
+}
+
+export class Mongo {
+
+    public static getCollectionName (): string {
+        throw Error('Undefined static method "getCollectionName"');
+    }
+
+    public static getSchema (): ISchema {
+        throw new Error('Undefined static method "getSchema"');
+    }
+
+    private _schema: ISchema = {};
+    private _collection: Collection;
+    private _db: Db;
+
+    constructor (options: IConfig) {
+        this._db = options.db;
+        this._schema = (this.constructor as typeof Mongo).getSchema();
+        this._collection = this._db.collection((this.constructor as typeof Mongo).getCollectionName());
+    }
+
+    public getObjectID (id: ObjectID | string): ObjectID | undefined {
+        return ObjectID.isValid(id) ? new ObjectID(id) : undefined;
+    }
+
+    public getObjectId (id: ObjectID | string): ObjectID | undefined {
+        return this.getObjectID(id);
+    }
+
+    public getDb (): Db {
+        return this._db;
+    }
+
+    public getCollection (): Collection {
+        return this._collection;
+    }
+
+    public async find (query: IQuery, options: IFindOptions = {}): Promise<Cursor | IResult[]> {
+        const cursor = this._collection.find(
+            await this._getJoinQuery(this.constructor as typeof Mongo, query, options.join),
+            options
+        );
+        const data = options.cursor ? cursor : await cursor.toArray();
+        return options.join ? await this.attachObjectIdData(data, options.join) : data;
+    }
+
+    public async attachObjectIdData (data: Cursor | IResult[], joins: IJoin[]): Promise<Cursor | IResult[]> {
+        if (data.constructor.name === 'Cursor') {
+            return (data as Cursor).map(
+                async (row: IResult) => {
+                    await this._attachDataToReferencePointer(
+                        row,
+                        joins,
+                        {
+                            type: DataType.OBJECT,
+                            object: this._schema
+                        }
+                    );
+                    return row;
+                }
+            );
+        } else {
+            await this._attachDataToReferencePointer(
+                data,
+                joins,
+                {
+                    type: DataType.OBJECT,
+                    object: this._schema,
+                    array: data.constructor.name === 'Array'
+                }
+            );
+        }
+        return data;
+    }
+
+    private async _attachDataToReferencePointer (
+        data: IResult[] | IResult,
+        joins: IJoin[],
+        fieldSchema: IFieldSchema,
+        model: typeof Mongo = this.constructor as typeof Mongo
+    ): Promise<void> {
+        const fieldPathsInJoin = joins.reduce((fieldPaths, join) => {
+            return fieldPaths.concat(toArray(join.on));
+        }, [] as string[]);
+
+        const objectIdInData: IObjectIdInData = {};
+        this._findObjectIdInData(data, fieldSchema, fieldPathsInJoin, objectIdInData);
+
+        if (Object.keys(objectIdInData).length === 0) {
+            return;
+        }
+
+        for (const join of joins) {
+            const fields = toArray(join.on);
+            const joinModel = join.model || this._findObjectIdFieldModel(fields[0], model);
+
+            const referencePointers: IObjectIdInData = {};
+            const ids: ObjectID[] = [];
+            // find all ids and create an object of reference pointers indexed by id
+            fields.forEach((field) => {
+                // field could be in join but not in the projection of the upper level
+                // example in test case: should filter resursive object id referenced fields when there are multiple matches
+                if (objectIdInData[field]) {
+                    objectIdInData[field].forEach((referencePointer) => {
+                        const id = (referencePointer.parent as any)[referencePointer.field]._id;
+                        if (!referencePointers[id]) {
+                            referencePointers[id] = [];
+                        }
+                        referencePointers[id].push(referencePointer);
+                        ids.push(id);
+                    });
+                }
+            });
+
+            let joinData = join.data;
+            if (!joinData) {
+                const projection = join.projection;
+                if (projection && projection._id === 0) {
+                    projection._id = 1;
+                }
+
+                joinData = this._db.collection(joinModel.getCollectionName()).find(
+                    {
+                        _id: {
+                            $in: ids
+                        }
+                    },
+                    { projection }
+                );
+            }
+
+            // if else here is for looping cursor only once to attach data
+            if (join.join) {
+                await this._attachDataToReferencePointer(
+                    await joinData.map((row: IResult) => {
+                        // field could be in join but not in the projection of the upper level
+                        // example in test case: should filter resursive object id referenced fields when there are multiple matches
+                        if (referencePointers[row._id]) {
+                            referencePointers[row._id].forEach((referencePointer) => {
+                                (referencePointer.parent as any)[referencePointer.field] = row;
+                            });
+                        }
+                        return row;
+                    }).toArray(),
+                    join.join,
+                    {
+                        type: DataType.OBJECT,
+                        object: joinModel.getSchema(),
+                        array: true
+                    },
+                    joinModel
+                );
+            } else {
+                await new Promise((resolve) => {
+                    (joinData as Cursor).forEach(
+                        (row) => {
+                            // field could be in join but not in the projection of the upper level
+                            // example in test case: should filter resursive object id referenced fields when there are multiple matches
+                            if (referencePointers[row._id]) {
+                                referencePointers[row._id].forEach((referencePointer) => {
+                                    (referencePointer.parent as any)[referencePointer.field] = row;
+                                });
+                            }
+                        },
+                        () => resolve()
+                    );
+                });
+            }
+        }
+    }
+
+    private _findObjectIdInData (
+        data: any,
+        fieldConfig: IFieldSchema,
+        fieldPathsInJoin: string[],
+        objectIdInData: IObjectIdInData,
+        referencePointer?: IReferencePointer
+    ): void {
+        if (fieldConfig) { // _id field and auto generated fields (e.g.dateCreated etc) do not have fieldConfig values.
+            if (fieldConfig.array) {
+                toArray(data).forEach((row, index) => {
+                    this._findObjectIdInData(
+                        row,
+                        {
+                            ...fieldConfig,
+                            array: false
+                        },
+                        fieldPathsInJoin,
+                        objectIdInData,
+                        {
+                            parent: data,
+                            field: index,
+                            path: referencePointer && referencePointer.path || []
+                        }
+                    );
+                });
+            } else {
+                switch (fieldConfig.type) {
+                    case DataType.OBJECT:
+                        if (!fieldConfig.object) {
+                            throw new Error(
+                                `Undefined "object" property on "${fieldConfig.type}" type in ${JSON.stringify(fieldConfig)}`
+                            );
+                        }
+                        for (const field in data) {
+                            this._findObjectIdInData(
+                                data[field],
+                                fieldConfig.object[field],
+                                fieldPathsInJoin,
+                                objectIdInData,
+                                {
+                                    parent: data,
+                                    field,
+                                    path: toArray(referencePointer && referencePointer.path).concat(field)
+                                }
+                            );
+                        }
+                        break;
+                    case DataType.OBJECTID:
+                        if (data && referencePointer) {
+                            if (!fieldConfig.model) {
+                                throw new Error(
+                                    `Undefined "model" property on "${fieldConfig.type}" type in ${JSON.stringify(fieldConfig)}`
+                                );
+                            }
+
+                            const fieldPath = referencePointer.path.join('.');
+                            if (fieldPathsInJoin.includes(fieldPath)) {
+                                const collection = fieldConfig.model.getCollectionName();
+
+                                if (!objectIdInData[fieldPath]) {
+                                    objectIdInData[fieldPath] = [];
+                                }
+                                // data.oid = DbRef type in mongo
+                                // const id = data.constructor.name === 'ObjectID' ? data : data.oid;
+                                // if (!objectIdInData[fieldPath][id]) {
+                                //     objectIdInData[fieldPath][id] = [];
+                                // }
+
+                                // clear object id data, if refereced data is not found,
+                                // the result will be { _id: ObjectId(...) } instead of the origin ObjectID or DBRef
+                                (referencePointer.parent as any)[referencePointer.field] = {
+                                    _id: data.constructor.name === 'ObjectID' ? data : data.oid
+                                };
+                                objectIdInData[fieldPath].push(referencePointer);
+
+                                // if (!objectIdInData[collection]) {
+                                //     objectIdInData[collection] = {
+                                //         model: fieldConfig.model,
+                                //         dbRefsById: {}
+                                //     };
+                                // }
+
+                                // // data.oid = DbRef type in mongo
+                                // const id = data.constructor.name === 'ObjectID' ? data : data.oid;
+                                // if (!objectIdInData[collection].dbRefsById[id]) {
+                                //     objectIdInData[collection].dbRefsById[id] = [];
+                                // }
+                                // objectIdInData[collection].dbRefsById[id].push(referencePointer);
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    private async _getJoinQuery (model: typeof Mongo, query: IQuery = {}, joins?: IJoin[]): Promise<IQuery> {
+        if (query && typeof(query._id) === 'string') {
+            query._id = this.getObjectID(query._id);
+        }
+
+        if (joins) {
+            const joinQuery: IQuery = {
+                $and: []
+            };
+
+            for (const join of joins) {
+                const fields = toArray(join.on);
+                join.model = this._findObjectIdFieldModel(fields[0], model);
+                const filters = await this._getJoinQuery(join.model, join.filters, join.join);
+
+                if (Object.keys(filters).length) {
+                    const projection = join.projection || {};
+                    if (projection._id === 0) {
+                        projection._id = 1;
+                    }
+
+                    const cursor = this._db.collection((join.model as typeof Mongo).getCollectionName() as string).find(
+                        filters,
+                        {
+                            projection
+                        }
+                    );
+
+                    const ids = (await cursor.toArray()).map((row) => row._id);
+                    fields.forEach((field) => {
+                        joinQuery.$and.push({
+                            [field]: {
+                                $in: ids
+                            }
+                        });
+                    });
+
+                    cursor.rewind();
+                    join.data = cursor;
+                }
+            }
+
+            if (joinQuery.$and.length) {
+                if (query && Object.keys(query).length) {
+                    joinQuery.$and.push(query);
+                }
+                return joinQuery;
+            }
+        }
+
+        return query;
+    }
+
+    private _findObjectIdFieldModel (fieldPath: string, model: typeof Mongo): typeof Mongo {
+        const fields = fieldPath.split('.');
+        let schema: ISchema = model.getSchema();
+        while (fields.length > 1) {
+            const field = fields.shift() as string;
+            if (schema[field].object) {
+                schema = schema[field].object as ISchema;
+            } else {
+                throw new Error(
+                    'Undefined "model" property or Invalid Object ID field in join statement.\n'
+                    + `On: "${fieldPath}"\n`
+                    + `Collection: ${model.getCollectionName()}\n`
+                    + `Schema: ${JSON.stringify(model.getSchema())}\n`
+                );
+            }
+        }
+
+        const objectIdField = fields.shift();
+        const fieldModel = objectIdField && schema && schema[objectIdField] && schema[objectIdField].model;
+        if (fieldModel) {
+            return fieldModel;
+        }
+        throw new Error(
+            '\nUndefined "model" property or Invalid Object ID field in join statement.\n'
+            + `On: "${fieldPath}"\n`
+            + `Collection: ${model.getCollectionName()}\n`
+            + `Schema: ${JSON.stringify(model.getSchema())}\n`
+        );
+    }
+
+}
+
+export default Mongo;
